39263b626f8f525fa4de5695ba8afeb33aef9d79
Richard Torres <torresrichard252@gmail.com>
2021-02-16 10:36:51 -0500

    Data and

--- FantasyFD.Data/Team.cs
+++ FantasyFD.Data/Team.cs
@@ -33,7 +33,6 @@ namespace FantasyFD.Data
     }
 
 
-    // NEEDS TO BE DELETED PRIOR TO INITIAL MERGE WITH GROUP
     public class Players { } // NEEDS TO BE DELETED PRIOR TO INITIAL MERGE WITH GROUP
     public class Games { } // NEEDS TO BE DELETED PRIOR TO INITIAL MERGE WITH GROUP
 
