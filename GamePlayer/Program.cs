691d78eab7a28856618c960da5740ac4611bd14b
Sabrina Jimenez <ReviverGirl@outlook.com>
2021-03-04 01:30:25 -0500

    oofs2.0

---
+++ GamePlayer/Program.cs
@@ -0,0 +1,35 @@
+﻿using FantasyFD.GamePlayer;
+using System;
+
+namespace Fantasy.GamePlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    HttpClient httpClient = new HttpClient();
+            //    HttpResponseMessage response = httpClient.GetAsync("https://localhost:44363/api/team/1").Result;
+            //    Console.WriteLine((int)response.StatusCode);
+            //    if (response.IsSuccessStatusCode)
+            //    {
+            //        Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            //        string colts = response.Content.ReadAsStringAsync().Result;
+            //        string jsonFormatted = JValue.Parse(colts).ToString(Formatting.Indented);
+            //        Console.WriteLine(jsonFormatted);
+            //        Team teamResponse = response.Content.;
+            //        Console.WriteLine($"{teamResponse.TeamName}");
+
+            //foreach (string teamURL in teamResponse.TeamName)
+            //{
+            //    Console.WriteLine(teamURL);
+            //    HttpResponseMessage teamResponse1 = httpClient.GetAsync(teamURL).Result;
+            //    Console.WriteLine(teamResponse1.Content.ReadAsStringAsync().Result);
+            //    Team team = teamResponse1.Content.ReadAsAsync<Team>().Result;
+            //    Console.WriteLine($"{team.TeamID} was directed by {team.TeamName}");
+            //}
+            //}
+            ProgramUI game = new ProgramUI();
+            game.Run();
+        }
+    }
+}
